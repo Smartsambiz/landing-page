5fe4528e54c7752984120ffeef2ec925ed1540f2
samuel <smartsam9981@gmail.com>
2023-03-19 14:58:58 +0100

    my landing page assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function add7(number){
+return number + 7
+}
+function multiply(a, b){
+    return a * b;
+}
+
+function capitalize(name){
+    return name.slice(5)
+}
+console.log(capitalize('samuel'));
